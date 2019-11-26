24c612b43e742f33a24734fdc95d17bbd93f5baa
Jesse McCullough <jesse.mccullough@gmail.com>
2019-11-26 00:55:14 -0500

    add place holder for /cats/index, /cats/show

--- routes/cats.js
+++ routes/cats.js
@@ -1,8 +1,13 @@
 var express = require("express");
-    router  = express.Router();
+router = express.Router();
 
-router.get("/", function(req,res){
-    res.render("cats/index");
+router.get("/", function(req, res) {
+  res.render("cats/index");
+});
+
+router.get("/cat", function(req, res) {
+  res.render("cats/show");
+  console.log("here's a cat");
 });
 
 module.exports = router;
